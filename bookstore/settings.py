a9edecc02801db9bb71bdf417d9d5d60787d6e26
JoaoVBonifácio <jboni2014@gmail.com>
2025-05-19 13:50:57 -0300

    Update settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Colocar o rest_framework aqui em baixo
     'rest_framework',
 ]
 
