64f3082c8e681b3642d7ec83df7fa383492bfe07
JoaoVBonifácio <jboni2014@gmail.com>
2025-05-23 13:43:00 -0300

    4

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -127,11 +127,11 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-INTERNAL_IPS = [
-    '127.0.0.1',
-]
-
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
