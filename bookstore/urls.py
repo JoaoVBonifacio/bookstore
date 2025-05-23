e03b4dbe09bb2ea61fffe835856f29d347660748
JoaoVBonifácio <jboni2014@gmail.com>
2025-05-23 13:36:12 -0300

    3

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 
 import debug_toolbar
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 urlpatterns = [
     path('__debug__', include(debug_toolbar.urls)),
     path("admin/", admin.site.urls),
+    
 ]
